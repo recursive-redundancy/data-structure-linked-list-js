a49d0de940dc221f1ffa8173fbda6c31b2787b4c
recursive-redundancy <danielmorgan.dev@gmail.com>
2019-08-04 17:09:51 -0500

    removed problem text from script (moved to README)

--- linked-list.js
+++ linked-list.js
@@ -1,17 +1,6 @@
 /*
-*
-*
-Implement a LinkedList class without using JavaScript’s built-in arrays ([]). Your LinkedList should support just 2 methods: add and has:
-
-class LinkedList {...}
-let list = new LinkedList(1, 2, 3)
-list.add(4)                           // undefined
-list.add(5)                           // undefined
-list.has(1)                           // true
-list.has(4)                           // true
-list.has(6)                           // false
-*
-*
+* Linked List - Javascript
+* solution by Daniel Morgan
 */
 
 class LinkedList {
